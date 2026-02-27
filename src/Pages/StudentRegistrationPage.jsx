e4cf28f5153dec9bd85b3ad271007aaf55295405
Aditya-dev979 <adityajangir979@gmail.com>
2026-02-27 17:36:48 +0530

    Delete src directory

--- src/Pages/StudentRegistrationPage.jsx
+++
@@ -1,9 +0,0 @@
-import StudentRegistration from "../Components/StudentRegistration";
-
-export default function StudentRegistrationPage() {
-  return (
-    <div className="pt-24">
-      <StudentRegistration />
-    </div>
-  );
-}
